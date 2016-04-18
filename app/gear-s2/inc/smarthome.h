07211d0b01ebeb10007e50cf531ba4cfea508401
sangho park <gouache95@gmail.com>
2016-04-19 08:45:24 +0900

    add initial gear s2 app

---
+++ app/gear-s2/inc/smarthome.h
@@ -0,0 +1,19 @@
+#ifndef __smarthome_H__
+#define __smarthome_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "smarthome"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.hobby-poroject.smarthome"
+#endif
+
+#endif /* __smarthome_H__ */
